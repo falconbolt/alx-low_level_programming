af409ccd1bfde72815ef45d43c2901308a6b10b5
Salmanu Usman <usalmanu1@gmail.com>
2022-09-09 04:47:34 -0700

    ERROR: trailing whitespace bug fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,35 +1,41 @@
-/*
- * File: 100-print_comb3.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
-
 /**
- * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
- *
- * Return: Always 0.
+ * main - main block
+ * Description: Print all possible combinations of two digits.
+ * Numbers must be separated by commas and a space.
+ * 01 and 10 are considered as the same combination of the two digits.
+ * Print only the smallest combination of two digits.
+ * Numbers should be printed in ascending order, with two digits.
+ * You can only use `putchar`.
+ * You can only use `putchar` up to 5 times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
  */
 int main(void)
 {
-	int digit1, digit2;
+	int i, j, k;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	i = 0;
+
+	while (i < 100)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		j = i % 10; /* singles digit */
+		k = i / 10; /* doubles digit */
+
+		if (k < j)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+			putchar(k + '0');
+			putchar(j + '0');
 
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-			
-			putchar(',');
-			putchar(' ');
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
 		}
-	}
 
+		i++;
+	}
 	putchar('\n');
 
 	return (0);
