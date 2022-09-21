1b365a667e502ae7e1fd8714e031d44bd2a6b6c6
falconbolt <111567542+falconbolt@users.noreply.github.com>
2022-09-21 19:23:13 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,19 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i, c;
 
-	while (dest[index++])
-		dest_len++;
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	for (c = 0; src[c] != '\0'; c++)
+	{
+		dest[i] = src[c];
+		i++;
+	}
 
+	dest[i] = '\0';
 	return (dest);
 }
