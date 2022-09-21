03ee690f149e69e3dce7c8ee736bca3ffc4e1de0
falconbolt <111567542+falconbolt@users.noreply.github.com>
2022-09-21 19:54:35 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	char *destTemp;
+	char *srcTemp;
 
-	int index = 0, dest_len = 0;
+	destTemp = dest;
+	srcTemp =  src;
 
-	while (dest[index++])
-		dest_len++;
+	while (*destTemp != '\0')
+		destTemp++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (*srcTemp != '\0')
+		*destTemp++ = *srcTemp++;
 
 	return (dest);
 }
