40bca8d26df0a60145d6581e951c10e132cdce71
falconbolt <111567542+falconbolt@users.noreply.github.com>
2022-09-21 19:58:33 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
+	int index;
 
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
+	for (index = 0; src[index] != '\0' && index < n; index++)
 		dest[index] = src[index];
 
-	for (index = src_len; index < n; index++)
+	for (; index < n; index++)
 		dest[index] = '\0';
-
 	return (dest);
 }
