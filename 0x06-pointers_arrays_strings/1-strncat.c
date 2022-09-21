e8f340b0719e2bba6a924a8a09facbc73d72613a
falconbolt <111567542+falconbolt@users.noreply.github.com>
2022-09-21 19:25:00 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,13 +11,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int i, c;
 
-	while (dest[index++])
-		dest_len++;
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
+	for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
+	{
+		dest[i] = src[c];
+	}
 	return (dest);
 }
