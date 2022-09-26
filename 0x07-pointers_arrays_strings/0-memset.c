2e02eefb161f148b5494ab8cae8c681656ad7af9
falconbolt <111567542+falconbolt@users.noreply.github.com>
2022-09-26 17:50:42 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,16 +6,6 @@
  * @s: A pointer to the memory area to be filled.
  * @c: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
- *void *_memset(void *s, int c, size_t n)
- *{
- *	unsigned int index;
- *	unsigned char *memory = s, value = c;
- *
- *	for (index = 0; index < n; index++)
- *		memory[index] = value;
- *
- *	return (memory);
- *}
  * Return: A pointer to the filled memory area @s.
  */
 char *_memset(char *s, char b, unsigned int n)
