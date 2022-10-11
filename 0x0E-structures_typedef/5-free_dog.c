95bec02e10cc9782bb9580c4b0edee064cbe0bee
falconbolt <111567542+falconbolt@users.noreply.github.com>
2022-10-11 18:26:58 +0100

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 
 /**
- * free_dog - Frees dogs.
- * @d: The dog to be freed.
+ * free_dog - free memory
+ *@d: array
+ * Description: Free memory for struct
  */
+
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-
-	free(d->owner);
-	free(d->name);
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
